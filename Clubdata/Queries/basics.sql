4a19dcdaf41126bb57e391ed7bb5b6fb27312023
elodiebhs <elodiebouthors@hotmail.com>
2021-07-21 20:53:38 +0000

    work on more basic queries

--- Clubdata/Queries/basics.sql
+++ Clubdata/Queries/basics.sql
@@ -18,4 +18,17 @@ SELECT * FROM facilities
 WHERE name like '%Tennis%';
 
 --retrieve the details of facilities with ID 1 and 5
+SELECT * FROM facilities
+WHERE facid IN (1,5); 
 
+--produce a list of facilities, with each labelled as 'cheap' or 'expensive' depending on if their monthly maintenance cost is more than $100. Return the name and monthly maintenance of the facilities in question.
+SELECT name, 
+	case when (monthlymaintenance > 100) then 'expensive'
+	else 'cheap'
+	end as cost 
+
+FROM facilities;
+
+-- produce a list of members who joined after the start of September 2012. Return the memid, surname, firstname, and joindate of the members in question.
+SELECT memid, surname, firstname, joindate FROM members
+WHERE joindate >= '2012-09-01'; 
