211b4f16e123043db4a6b8fe392d9eb1ec0b7141
elodiebhs <elodiebouthors@hotmail.com>
2021-07-21 21:36:40 +0000

    Work out the start times of bookings for tennis courts

--- Clubdata/Queries/join.sql
+++ Clubdata/Queries/join.sql
@@ -2,3 +2,10 @@
 SELECT starttime FROM bookings
 JOIN members ON members.memid = bookings.memid
 WHERE members.firstname = 'David' AND members.surname = 'Farrell';
+
+--produce a list of the start times for bookings for tennis courts, for the date '2012-09-21'. Return a list of start time and facility name pairings, ordered by the time.
+SELECT starttime as start, name FROM bookings
+JOIN facilities ON bookings.facid = facilities.facid
+WHERE name LIKE '%Tennis Court%'
+AND starttime >= '2012-09-21' AND starttime < '2012-09-22'
+ORDER BY start;
