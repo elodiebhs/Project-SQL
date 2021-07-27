9ff2a5ad0e8311c74b739ec1bf9f0db73cafe7e2
elodiebhs <elodiebouthors@hotmail.com>
2021-07-27 22:09:06 +0000

    Insert multiple rows of data into a table

--- Clubdata/Queries/modifying_data.sql
+++ Clubdata/Queries/modifying_data.sql
@@ -1,3 +1,7 @@
 --Insert some data into table facilities
 
-INSERT INTO cd.facilities (facid, name, membercost, guestcost, initialoutlay, monthlymaintenance) VALUES (9, 'Spa', 20, 30, 100000, 800);  
+INSERT INTO facilities (facid, name, membercost, guestcost, initialoutlay, monthlymaintenance) VALUES (9, 'Spa', 20, 30, 100000, 800);  
+
+--Insert multiple rows of data into a table
+
+INSERT INTO facilities VALUES (9, 'Spa', 20, 30, 100000, 800), (10, 'Squash Court 2', 3.5, 17.5, 5000, 80);       
