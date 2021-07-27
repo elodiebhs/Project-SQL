d583a4b634bb2d3465c99f0967eb3dc8ddabb2df
elodiebhs <elodiebouthors@hotmail.com>
2021-07-27 22:32:29 +0000

    Insert calculated data into a table

--- Clubdata/Queries/modifying_data.sql
+++ Clubdata/Queries/modifying_data.sql
@@ -4,4 +4,10 @@ INSERT INTO facilities (facid, name, membercost, guestcost, initialoutlay, month
 
 --Insert multiple rows of data into a table
 
-INSERT INTO facilities VALUES (9, 'Spa', 20, 30, 100000, 800), (10, 'Squash Court 2', 3.5, 17.5, 5000, 80);       
+INSERT INTO facilities VALUES (9, 'Spa', 20, 30, 100000, 800), (10, 'Squash Court 2', 3.5, 17.5, 5000, 80);
+
+--Insert calculated data into a table
+INSERT INTO facilities (facid, name, membercost, guestcost, initialoutlay, monthlymaintenance)
+SELECT  (
+SELECT  MAX(facid)
+FROM facilities) +1, 'Spa', 20, 30, 100000, 800;
