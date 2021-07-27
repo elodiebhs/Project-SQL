511c940e1d45f53e7c79b8cba11a13f8bf9a4557
elodiebhs <elodiebouthors@hotmail.com>
2021-07-27 22:42:55 +0000

    Update multiple rows and columns at the same time

--- Clubdata/Queries/modifying_data.sql
+++ Clubdata/Queries/modifying_data.sql
@@ -17,3 +17,10 @@ UPDATE facilities
 
 SET initialoutlay = 10000
 WHERE facid = 1; 
+
+--We want to increase the price of the tennis courts for both members and guests. Update the costs to be 6 for members, and 30 for guests.
+UPDATE facilities
+
+SET membercost = 6, guestcost = 30
+WHERE facid IN (0, 1); 
+
