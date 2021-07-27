d64e588d53e9cc95aa476eb06080465aa547353f
elodiebhs <elodiebouthors@hotmail.com>
2021-07-27 21:59:09 +0000

    Insert some data into table facilities

---
+++ Clubdata/Queries/modifying_data.sql
@@ -0,0 +1,3 @@
+--Insert some data into table facilities
+
+INSERT INTO cd.facilities (facid, name, membercost, guestcost, initialoutlay, monthlymaintenance) VALUES (9, 'Spa', 20, 30, 100000, 800);  
