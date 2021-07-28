1dc1f7377ce772c3b8a933092819b386bfbde1c7
elodiebhs <elodiebouthors@hotmail.com>
2021-07-28 18:08:06 +0000

    Count the number of expensive facilities

--- Clubdata/Queries/aggregation.sql
+++ Clubdata/Queries/aggregation.sql
@@ -1,2 +1,6 @@
 --We want to know how many facilities exist
 SELECT COUNT (*) FROM facilities;
+
+--Produce a count of the number of facilities that have a cost to guests of 10 or more.
+
+SELECT COUNT(*) FROM facilities WHERE guestcost >= 10;
