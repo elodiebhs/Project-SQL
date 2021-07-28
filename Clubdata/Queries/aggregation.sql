ef08d7cbec6382e7bdcaab8aa039404abd7c3852
elodiebhs <elodiebouthors@hotmail.com>
2021-07-28 18:42:35 +0000

    count of members who have made at least one booking

--- Clubdata/Queries/aggregation.sql
+++ Clubdata/Queries/aggregation.sql
@@ -1,45 +1,43 @@
 --We want to know how many facilities exist
 SELECT  COUNT (*)
 FROM facilities;
-
 --Produce a count of the number of facilities that have a cost to guests of 10 or more.
 SELECT  COUNT(*)
 FROM facilities
-WHERE guestcost >= 10; 
-
---Produce a count of the number of recommendations each member has made. Order by member ID.
-SELECT  recommendedby
+WHERE guestcost >= 10;
+--Produce a count of the number of recommendations each member has made.
+ORDER BY member ID. 
+SELECT  recommendedby 
        ,COUNT(*)
 FROM members
 WHERE recommendedby IS NOT NULL 
 GROUP BY  recommendedby
 ORDER BY recommendedby;
-
-
 --Produce a list of the total number of slots booked per facility.
-SELECT  facid
+SELECT  facid 
        ,SUM(slots) AS "Total Slots"
 FROM bookings
 GROUP BY  facid
 ORDER BY facid;
-
---Produce a list of the total number of slots booked per facility in the month of September 2012
-SELECT  facid
+--Produce a list of the total number of slots booked per facility IN the month of September 2012
+SELECT  facid 
        ,SUM(slots) AS "Total Slots"
 FROM bookings
 WHERE starttime >= '2012-09-01' 
 AND starttime < '2012-10-01' 
 GROUP BY  facid
-ORDER BY SUM(slots); 
-
---Produce a list of the total number of slots booked per facility per month in the year of 2012
-SELECT  facid
+ORDER BY SUM(slots);
+--Produce a list of the total number of slots booked per facility per month IN the year of 2012
+SELECT  facid 
        ,extract(month
 FROM starttime) AS month, SUM(slots) AS "Total Slots"
 FROM bookings
 WHERE extract(year 
 FROM starttime) = 2012
-GROUP BY  facid
+GROUP BY  facid 
          ,month
-ORDER BY facid
+ORDER BY facid 
          ,month;
+--Find the total number of members （including guests） who have made at least one booking.
+SELECT  COUNT(DISTINCT memid)
+FROM bookings;
