367f5f112fcaecb5dc8f3bc8d0b8720123eab639
elodiebhs <elodiebouthors@hotmail.com>
2021-07-28 18:24:50 +0000

    List the total slots booked per facility in a given month

--- Clubdata/Queries/aggregation.sql
+++ Clubdata/Queries/aggregation.sql
@@ -1,17 +1,33 @@
 --We want to know how many facilities exist
-SELECT COUNT (*) FROM facilities;
+SELECT  COUNT (*)
+FROM facilities;
 
 --Produce a count of the number of facilities that have a cost to guests of 10 or more.
-SELECT COUNT(*) FROM facilities WHERE guestcost >= 10;
+SELECT  COUNT(*)
+FROM facilities
+WHERE guestcost >= 10; 
 
 --Produce a count of the number of recommendations each member has made. Order by member ID.
-SELECT recommendedby, COUNT(*) FROM members
-WHERE recommendedby IS NOT NULL
-GROUP BY recommendedby
+SELECT  recommendedby
+       ,COUNT(*)
+FROM members
+WHERE recommendedby IS NOT NULL 
+GROUP BY  recommendedby
 ORDER BY recommendedby;
 
 
 --Produce a list of the total number of slots booked per facility.
-SELECT facid, SUM(slots) AS "Total Slots" FROM bookings
-GROUP BY facid
+SELECT  facid
+       ,SUM(slots) AS "Total Slots"
+FROM bookings
+GROUP BY  facid
 ORDER BY facid;
+
+--Produce a list of the total number of slots booked per facility in the month of September 2012
+SELECT  facid
+       ,SUM(slots) AS "Total Slots"
+FROM bookings
+WHERE starttime >= '2012-09-01' 
+AND starttime < '2012-10-01' 
+GROUP BY  facid
+ORDER BY SUM(slots); 
